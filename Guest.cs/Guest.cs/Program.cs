9bc2c1af9fe6996484226ecc0cdbc773e86fcda0
KanokphonToNG <kanokphon.l@kkumail.com>
2026-02-24 23:48:55 +0700

    Add files via upload

---
+++ Guest.cs/Guest.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class Guest : User, Member
+{
+    public Guest(string name, string email)
+        : base(name, email)
+    {
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Guest: {Name}, Email: {Email}");
+    }
+
+    public void Register()
+    {
+        Console.WriteLine($"{Name} (Guest) registered successfully.");
+    }
+
+    public void ShowRole()
+    {
+        Console.WriteLine("Role: Guest");
+    }
+}
