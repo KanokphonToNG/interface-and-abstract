3f65d75882b86724a9fb58bdc4760f21a5f9d50c
KanokphonToNG <kanokphon.l@kkumail.com>
2026-02-24 23:50:55 +0700

    Add files via upload

---
+++ Student.cs/Student.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class Student : User, Member
+{
+    public string StudentID { get; set; }
+
+    public Student(string name, string email, string studentID)
+        : base(name, email)
+    {
+        StudentID = studentID;
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Student: {Name}, ID: {StudentID}, Email: {Email}");
+    }
+
+    public void Register()
+    {
+        Console.WriteLine($"{Name} (Student) registered successfully.");
+    }
+
+    public void ShowRole()
+    {
+        Console.WriteLine("Role: Student");
+    }
+}
