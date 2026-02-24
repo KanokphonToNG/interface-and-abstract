a88aa1317821b2b2e5b52054fa5552ba5ed13446
KanokphonToNG <kanokphon.l@kkumail.com>
2026-02-24 23:53:17 +0700

    Add files via upload

---
+++ Teacher.cs/Teacher.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+
+public class Teacher : User, Member
+{
+    public string Position { get; set; }
+
+    public Teacher(string name, string email, string position)
+        : base(name, email)
+    {
+        Position = position;
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Teacher: {Name}, Position: {Position}, Email: {Email}");
+    }
+
+    public void Register()
+    {
+        Console.WriteLine($"{Name} (Teacher) registered successfully.");
+    }
+
+    public void ShowRole()
+    {
+        Console.WriteLine("Role: Teacher");
+    }
+}
