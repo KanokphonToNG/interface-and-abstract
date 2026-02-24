cf6b0cc3298a36c6f2fdd72fd951ff4a7fbbadac
KanokphonToNG <kanokphon.l@kkumail.com>
2026-02-24 23:50:26 +0700

    Add files via upload

---
+++ StarterMember.cs/StarterMember.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StarterMember
+{
+    private List<User> members = new List<User>();
+
+    public void AddMember(User user)
+    {
+        members.Add(user);
+        Console.WriteLine("Member added successfully.\n");
+    }
+
+    public void ShowAllMembers()
+    {
+        Console.WriteLine("=== Member List ===");
+        foreach (var m in members)
+        {
+            m.ShowInfo();
+        }
+    }
+}
