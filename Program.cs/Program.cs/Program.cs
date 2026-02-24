6bfbd55858319dc3d03c783894c639c165d7f526
KanokphonToNG <kanokphon.l@kkumail.com>
2026-02-24 23:49:57 +0700

    Add files via upload

---
+++ Program.cs/Program.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StarterMember system = new StarterMember();
+
+        Student s1 = new Student("Anan", "anan@email.com", "S001");
+        Teacher t1 = new Teacher("Suda", "suda@email.com", "Professor");
+        Guest g1 = new Guest("John", "john@email.com");
+
+        s1.Register();
+        t1.Register();
+        g1.Register();
+
+        system.AddMember(s1);
+        system.AddMember(t1);
+        system.AddMember(g1);
+
+        system.ShowAllMembers();
+
+        Console.ReadLine();
+    }
+}
